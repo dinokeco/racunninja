6d3b22db931b80957593a8caa0a415050a920a8a
dinokeco <dino.keco@gmail.com>
2018-03-13 09:34:54 +0100

    align login form

--- examples/controllers/sidebar-controller.js
+++ examples/controllers/sidebar-controller.js
@@ -26,13 +26,3 @@ app.controller('sidebarCtrl', function($scope, $location){
     }
 
 });
-
-/*
-function SidebarController($scope, $location){
-    console.log("Hello from angular controller");
-
-    $scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
-    };
-    $scope.dashboard_active = 'active'; 
-}*/
